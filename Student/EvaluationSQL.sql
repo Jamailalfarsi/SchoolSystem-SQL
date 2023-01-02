47d40b108b399d0854ce9c88402de9c40c7e67cc
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-01-02 09:08:56 +0400

    Evaluation Connection SQL with java

---
+++ Student/EvaluationSQL.sql
@@ -0,0 +1,49 @@
+use SchoolMgt;
+
+SELECT *FROM students;
+
+SELECT*FROM subjects;
+
+SELECT *FROM students;
+
+INSERT INTO students VALUE 
+(1,'Ali','Alfarsi','1998-06-1'),
+(2,'Sara','Alfarsi','1998-05-2'),
+(3,'Salim','Alfarsi','1999-05-2'),
+(4,'Maha','Alfarsi','1994-05-2'),
+(5,'Maha','Alfarsi','1994-05-2'),
+(6,'Maha','Alfarsi','1994-05-2'),
+(7,'Maha','Alfarsi','1994-05-2'),
+(8,'Maha','Alfarsi','1994-05-2'),
+(9,'Maha','Alfarsi','1994-05-2'),
+(10,'Maha','Alfarsi','1994-05-2'),
+(12,'Maha','Alfarsi','1994-05-2'),
+(13,'Maha','Alfarsi','1994-05-2'),
+(14,'Maha','Alfarsi','1994-05-2');
+
+Select*From students;
+
+INSERT INTO subjects VALUE 
+(1,'Math','fail','100.5'),
+(2,'English','successful','100.5'),
+(3,'Art','successful','100.6'),
+(4,'Math','successful','100.5'),
+(5,'Math','successful','100.5'),
+(6,'Math','successful','100.5'),
+(7,'Math','successful','100.5'),
+(8,'English','successful','100.5'),
+(9,'Math','successful','100.5'),
+(10,'Math','successful','100.5'),
+(11,'Math','successful','150.5'),
+(12,'Math','successful','100.5'),
+(13,'Math','successful','100.5'),
+(14,'Arabic','successful','100.5'),
+(15,'Math','successful','100.5'),
+(16,'Music','successful','100');
+
+SELECT *FROM subjects;
+
+
+
+
+
